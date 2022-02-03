dd68f24572a382ec2631f7826f399e9b703b55f2
jeansaigne <aymerictibere@gmail.com>
2022-02-03 19:01:21 +0100

    feat: Added account route auth guard Closes #22

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from "@auth0/auth0-angular";
 import { HomepageComponent } from "./homepage/homepage.component";
 import { UserProfileComponent } from "./user-profile/user-profile.component";
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
-  { path: 'account', component: UserProfileComponent },
+  { path: 'account', component: UserProfileComponent, canActivate: [AuthGuard] },
   { path: 'doc', loadChildren: () => import('./documentation/documentation.module').then((m) => m.DocumentationModule) },
 ];
 
